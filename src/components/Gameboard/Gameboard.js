ff1898188eec21cb174a7f6222a57237731d0389
Cody Clements <subliminalverses2@gmail.com>
2022-08-16 10:35:26 -0500

    create a functin that takes pokemon from api and shufeels them

--- src/components/Gameboard/Gameboard.js
+++ src/components/Gameboard/Gameboard.js
@@ -1,12 +1,42 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 
-function Gameboard(props){
+function Gameboard(props) {
+  const [pokemons, setPokemon] = useState([])
 
-   return (
-     <div className='gameboard'>
-       {}
-     </div>
-   )
+
+  useEffect(() => {
+    getPokeData()
+  }, [])
+
+  async function getPokeData() {
+
+    let newPokemons = []
+
+    for (let i = 1; i < 13 ; i++) {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+      const pokeData = await response.json();
+      const newPokemon = { name: pokeData.name }
+      newPokemons.push(newPokemon)
+    }
+
+    shufflePokemon(newPokemons)
+    setPokemon(newPokemons)
+  }
+
+  const shufflePokemon = pokemon => {
+  for (let i = pokemon.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = pokemon[i];
+    pokemon[i] = pokemon[j];
+    pokemon[j] = temp;
+  }
+}
+
+
+  return (
+    <div className='gameboard'>
+    </div>
+  )
 }
 
 export default Gameboard;
